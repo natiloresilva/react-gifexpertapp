669ca38adb42fe085a26a777e29919ba5903b01c
Natalia Silva <natalia.pupushi@gmail.com>
2020-10-15 05:35:53 -0300

    docs agregado

---
+++ docs/precache-manifest.d623706fd11b97b0bffa090bd672b6c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a108e0ea986401a7abaa17d1e69c189",
+    "url": "/index.html"
+  },
+  {
+    "revision": "773aa2c5d55bfed60114",
+    "url": "/static/css/main.b58d7156.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "773aa2c5d55bfed60114",
+    "url": "/static/js/main.ee718d96.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
